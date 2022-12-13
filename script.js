44c1286d3fe22ec31821d974958915385f8cc6d8
Alexandre <aleaxe7@gmail.com>
2022-12-12 21:37:53 -0300

    Added function to create books cards, added more styling to the page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookGrid = document.querySelector(".books-grid");
 
 function Book(name, author, numberOfPages, readStatus) {
     this.name = name;
@@ -9,4 +10,20 @@ function Book(name, author, numberOfPages, readStatus) {
 
 function addBookToLibrary(name, author, numberOfPages, readStatus) {
     return myLibrary.push(new Book(name, author, numberOfPages, readStatus));
+}
+
+function displayBook() {
+    bookGrid.replaceChildren();
+
+    myLibrary.forEach(book => {
+        let newBook = document.createElement("div");
+        newBook.classList.add("book");
+
+        Object.values(book).forEach(value => {
+            let bookInfo = document.createElement("p");
+            bookInfo.textContent = value;
+            newBook.appendChild(bookInfo);
+        }); 
+        bookGrid.appendChild(newBook);           
+    });
 }
