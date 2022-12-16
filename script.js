ae1164834326392172cb532094a08768a81391fd
Alexandre <aleaxe7@gmail.com>
2022-12-15 21:51:20 -0300

    Added more functions and styling

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const myLibrary = [];
 const bookGrid = document.querySelector(".books-grid");
 let numberOfBooks = document.querySelector("#num-of-books");
 numberOfBooks.textContent = myLibrary.length;
+const popupForm = document.querySelector(".form-container");
+
 
 
 function Book(name, author, numberOfPages, readStatus) {
@@ -11,10 +13,12 @@ function Book(name, author, numberOfPages, readStatus) {
     this.readStatus = readStatus;
 }
 
+
 function addBookToLibrary(name, author, numberOfPages, readStatus) {
     myLibrary.push(new Book(name, author, numberOfPages, readStatus));
     numberOfBooks.textContent = myLibrary.length;
 }
+
 
 function displayBook() {
     bookGrid.replaceChildren();
@@ -31,3 +35,18 @@ function displayBook() {
         bookGrid.appendChild(newBook);           
     });
 }
+
+const openFormButton = document.querySelector("#add-book");
+openFormButton.addEventListener("click", openForm) ;
+
+const closeFormButton = document.querySelector("#close-form");
+closeFormButton.addEventListener("click", closeForm);
+
+function openForm() {
+    popupForm.classList.add("open-form");
+}
+
+function closeForm() {
+    popupForm.classList.remove("open-form");
+}
+
