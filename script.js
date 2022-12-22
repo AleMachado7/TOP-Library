52a5306694698afce94101ea64b827c744907c62
Alexandre <aleaxe7@gmail.com>
2022-12-22 20:28:00 -0300

    Modify function to create book elements

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 const myLibrary = [];
 const bookGrid = document.querySelector(".books-grid");
 let numberOfBooks = document.querySelector("#num-of-books");
-numberOfBooks.textContent = myLibrary.length;
 const popupForm = document.querySelector(".form-container");
 let deleteButtons = document.querySelectorAll(".delete-book-button");
 
-function Book(title, author, numberOfPages, readStatus) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.numberOfPages = numberOfPages;
-  this.readStatus = readStatus;
+  this.pages = pages;
+  this.read = read;
+
+  function changeReadStatus() {
+    this.read = !this.read;
+  }
 }
 
-function addBookToLibrary(title, author, numberOfPages, readStatus) {
-  myLibrary.push(
-    new Book(
-      `Title: ${title}`,
-      `Author: ${author}`,
-      `Number of Pages: ${numberOfPages}`,
-      `Already read: ${readStatus}`
-    )
-  );
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
   numberOfBooks.textContent = myLibrary.length;
   displayBook();
-  deleteButtons = document.querySelectorAll(".delete-book-button");
 }
 
 function displayBook() {
@@ -34,15 +29,17 @@ function displayBook() {
     let deleteButton = document.createElement("button");
     deleteButton.classList.add("delete-book-button");
     deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", () =>
+    deleteButton.addEventListener("click", () => {
       deleteBook(deleteButton.parentElement)
-    );
-
-    Object.values(myLibrary[i]).forEach((value) => {
-      let bookInfo = document.createElement("p");
-      bookInfo.textContent = value;
-      newBook.appendChild(bookInfo);
     });
+
+    Object.entries(myLibrary[i]).forEach(([key, value]) => {
+      let dataLabel = key.charAt(0).toUpperCase() + key.slice(1);
+      let bookInfo = document.createElement("p");
+      bookInfo.textContent = dataLabel + ": " + value;
+      newBook.appendChild(bookInfo);
+      })
+
     newBook.setAttribute("data-index", i);
     newBook.appendChild(deleteButton);
     newBook.classList.add("book");
@@ -53,6 +50,7 @@ function displayBook() {
 function deleteBook(element) {
   let elementIndex = element.getAttribute("data-index");
   myLibrary.splice(elementIndex, 1);
+  numberOfBooks.textContent = myLibrary.length;
   displayBook();
 }
 
@@ -75,7 +73,7 @@ addBook.addEventListener("click", (event) => {
   let bookTitle = document.querySelector("#title").value;
   let bookAuthor = document.querySelector("#author").value;
   let bookPages = document.querySelector("#pages").value;
-  let bookRead = document.querySelector("#read").value;
+  let bookRead = document.querySelector("#read").checked;
 
   if (bookTitle === "" || bookAuthor === "" || bookPages === "") {
     event.preventDefault();
